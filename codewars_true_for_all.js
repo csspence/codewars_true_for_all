330020601a39b08ffd891c400dc13ecc7dc0234e
Crew Spence <crewspence@gCrewSpence.local>
2021-05-28 05:53:30 -0400

    Wrote working solution.

--- codewars_true_for_all.js
+++ codewars_true_for_all.js
@@ -2,6 +2,12 @@
 Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
 */
 
-function all( arr, fun ){
-  // ...
+const all = ( arr, fun ) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(fun(arr[i]) !== true) {
+      return false;
+    }
+  }
+
+  return true;
 }
