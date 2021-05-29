b9a7759ada690c257c82e1636503238253574330
Crew Spence <csspence123@gmail.com>
2021-05-29 04:39:27 -0400

    Update codewars_true_for_all.js

--- codewars_true_for_all.js
+++ codewars_true_for_all.js
@@ -1,5 +1,6 @@
 /*
-Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
+Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. 
+Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
 */
 
 const all = ( arr, fun ) => {
